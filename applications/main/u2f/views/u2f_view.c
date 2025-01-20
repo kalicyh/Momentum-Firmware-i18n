02e72b3632c329b8fd78392a2c57fe623eba940e
kalicyh <34980061+kaliCYH@users.noreply.github.com>
2025-01-21 01:29:29 +0800

    🦄 refactor(APP): U2F 汉化

--- applications/main/u2f/views/u2f_view.c
+++ applications/main/u2f/views/u2f_view.c
@@ -21,26 +21,24 @@ static void u2f_view_draw_callback(Canvas* canvas, void* _model) {
 
     if(model->display_msg == U2fMsgNotConnected) {
         canvas_draw_icon(canvas, 22, 15, &I_Connect_me_62x31);
-        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "Connect to a device");
+        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "连接到设备");
     } else if(model->display_msg == U2fMsgIdle) {
         canvas_draw_icon(canvas, 22, 15, &I_Connected_62x31);
-        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "Connected!");
+        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "已连接！");
     } else if(model->display_msg == U2fMsgRegister) {
-        elements_button_center(canvas, "OK");
+        elements_button_center(canvas, "确定");
         canvas_draw_icon(canvas, 22, 15, &I_Auth_62x31);
-        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "Press OK to register");
+        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "按确定进行注册");
     } else if(model->display_msg == U2fMsgAuth) {
-        elements_button_center(canvas, "OK");
+        elements_button_center(canvas, "确定");
         canvas_draw_icon(canvas, 22, 15, &I_Auth_62x31);
-        canvas_draw_str_aligned(
-            canvas, 128 / 2, 3, AlignCenter, AlignTop, "Press OK to authenticate");
+        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "按确定进行认证");
     } else if(model->display_msg == U2fMsgSuccess) {
         canvas_draw_icon(canvas, 22, 15, &I_Connected_62x31);
-        canvas_draw_str_aligned(
-            canvas, 128 / 2, 3, AlignCenter, AlignTop, "Authentication successful!");
+        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "认证成功！");
     } else if(model->display_msg == U2fMsgError) {
         canvas_draw_icon(canvas, 22, 15, &I_Error_62x31);
-        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "Certificate error");
+        canvas_draw_str_aligned(canvas, 128 / 2, 3, AlignCenter, AlignTop, "证书错误");
     }
 }
 
